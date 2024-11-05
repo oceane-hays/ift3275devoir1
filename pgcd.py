1b0042231b3996bf54d3f50d8e265f8c9fa04299
xichen816 <128641225+xichen816@users.noreply.github.com>
2024-11-04 23:50:46 -0500

    question 1.2

---
+++ pgcd.py
@@ -0,0 +1,19 @@
+def find_invpow(x,n):
+    """Finds the integer component of the n'th root of x,
+    an integer such that y ** n <= x < (y + 1) ** n.
+    """
+    high = 1
+    while high ** n <= x:
+        high *= 2
+    low = high/2
+    while low < high:
+        mid = (low + high) // 2
+        if low < mid and mid**n < x:
+            low = mid
+        elif high > mid and mid**n > x:
+            high = mid
+        else:
+            return mid
+    return mid + 1
+
+print(int(find_invpow(1101510739796100601351050380607502904616643795400781908795311659278941419415375,3)))
